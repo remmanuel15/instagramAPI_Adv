cf2338b348ccf2a735f33bbcd07e5328508984dc
remmanuel15 <remmanuel@cristoreyny.org>
2016-02-04 13:49:20 +0000

    Flicker Project

--- starter_code/js/script.js
+++ starter_code/js/script.js
@@ -1,6 +1,24 @@
 // YOUR CODE GOES HERE
-var clientID = "REPLACE ME WITH CLIENT ID";
+var clientID = "3565fe59fc166cb7f5a0ab9b7c982cc8";
 
 $(document).ready(function() {
+  $("#reset-btn").click(function() {
+                    $("#search-results").empty();
+  });
     // your code here
+    $("#search-btn").click(function() {
+        var cool = $("#search-UserInput").val();
+        searchFlickr(clientID, cool, 10, function(photos) {
+            console.log(photos.photo[0].url_m);
+
+
+            var i;
+            for (i = 0; i < photos.photo.length; i++) {
+                $("#search-results").append("<img src= " + photos.photo[i].url_m + ">");
+              
+
+             
+            };
+        });
+    });
 });
